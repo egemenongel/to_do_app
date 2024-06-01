44ed63d74f23e0bb574520dcd26db842c3098df6
egemenongel <egemenongel@hotmail.com>
2024-06-01 19:24:00 +0300

    feat: Create theme class

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'theme.g.dart';
+
+@riverpod
+ThemeData theme(ThemeRef ref) {
+  return AppTheme.lightTheme;
+}
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      colorScheme: const ColorScheme(
+        brightness: Brightness.light,
+        primary: Color(0xff3556AB),
+        onPrimary: Colors.white,
+        secondary: Color(0xff9EB031),
+        onSecondary: Colors.red,
+        error: Colors.red,
+        onError: Colors.white,
+        surface: Color(0xfff3f3f3),
+        onSurface: Colors.black,
+        tertiary: Color(0xff071D55),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:to_do_app/core/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ref.watch(themeProvider),
     );
   }
 }
